26d3b74cf9b9c15c9cf5c87a83490f2ce7ef22f3
Matias <Matias@DESKTOP-TA0TGEP>
2017-08-30 09:51:46 +0200

    tilføjet sortkaffe class og lavet unit test over den

---
+++ KaffeTests/KaffeTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe.Tests
+{
+    [TestClass()]
+    public class KaffeTests
+    {
+        [TestMethod()]
+        public void prisTestKaffe()
+        {
+            // Arrange
+            var Kaffe = new SortKaffe();
+            // Act
+            int pris = Kaffe.pris();
+            // Assert
+            Assert.AreEqual(20, pris);
+
+
+        }
+
+        [TestMethod()]
+        public void styrkeTestKaffe()
+        {
+            // Arrange
+            var Kaffe = new SortKaffe();
+            // Act
+            string styrke = Kaffe.styrke();
+            // Assert
+            Assert.AreEqual("stærk", styrke);
+
+        }
+    }
+}
